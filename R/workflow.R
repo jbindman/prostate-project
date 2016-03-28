cde78afb187197c45c3b0581f85fa5f2907803d8
Julia Bindman <jbindman@jhu.edu>
2016-03-28 13:41:39 -0400

    mon 3-28

--- R/workflow.R
+++ R/workflow.R
@@ -1,4 +1,4 @@
-rm(list=ls()) #clears
+#rm(list=ls()) #clears
 #TEST
 
 #setwd("Desktop/JHU/Prostate/prostate-project") #change to your working directory
@@ -9,7 +9,7 @@ source("R/fillPatientTables.R")
 #######################
 
 demo_data<-read.csv("julia-demo-data.csv")
-colnames(demo_data) <- c("X", "id", "dob") #customize (right now default)
+#colnames(demo_data) <- c("X", "id", "dob") #customize (right now default)
 
 psa_data<-read.csv("julia-psa-data.csv")
 #colnames(psa_data) <- c("X", "id", "psa", "psa.date") #customizable
@@ -20,7 +20,7 @@ bx_data<-read.csv("julia-bx-data.csv")
 tx_data<-read.csv("julia-tx-data.csv")
 #colnames(tx_data) <- c("X", "id", "GS", "tx.date") #customizable
 
-#all <- fillPatientTables(tx_data, demo_data, psa_data, bx_data) #builds three data frames
+all <- fillPatientTables(tx_data, demo_data, psa_data, bx_data) #builds three data frames
 
 
 
