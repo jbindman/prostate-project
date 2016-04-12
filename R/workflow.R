4a3c23dfa1c5816f8a95af02ce5d10e26ea5fac0
Julia Bindman <jbindman@jhu.edu>
2016-04-12 01:56:18 -0400

    tuesday

--- R/workflow.R
+++ R/workflow.R
@@ -1,13 +1,12 @@
 #rm(list=ls()) #clears
 
-#these four packages need to be installed with dependencies
+# 0. Install Packages with Dependencies
 install.packages("devtools")
-devtools::install_github("jbindman/prostate-project")
-
+devtools::install_github("jbindman/prostate-project") #install package
 install.packages("lme4", repos="http://cran.rstudio.com/")
 install.packages("splines", repos="http://cran.rstudio.com/") #not available R 3.2.3., supress warning
 install.packages("bayesm", repos="http://cran.rstudio.com/")
-install.packages("Matrix", repos="http://cran.rstudio.com/") #necessary? #double colon syntax?
+install.packages("Matrix", repos="http://cran.rstudio.com/") #double colon syntax?
 install.packages("rjags")
 
 #setwd("~/Desktop/JHU/Prostate/prostate-project") #change to your working directory
@@ -16,26 +15,33 @@ source("R/RJAGSprep.R")
 
 
 #######################
-# specify file names
+# #1 Customize File Names
+
 demo_data<-read.csv("julia-demo-data.csv")
 psa_data<-read.csv("julia-psa-data.csv")
 bx_data<-read.csv("julia-bx-data.csv")
 tx_data<-read.csv("julia-tx-data.csv")
-
-patientDataframes <- fillPatientTables(tx_data, demo_data, psa_data, bx_data)
-
 model.file <- "UNADJ-jags-model.txt"
+
+
+
+# #2 Prep for RJAGS
+patientDataframes <- fillPatientTables(tx_data, demo_data, psa_data, bx_data)
 RJAGSprepfull <- RJAGSprep(patientDataframes, model.file) #returns list of arguments for running RJAGS
 
 
-#if we want to call from this main workflow:
+# #3 Execute RJAGS (call from this main workflow? Within function?)
+
 jags_data <- RJAGSprepfull [[1]]
 inits <- RJAGSprepfull [[2]]
 parameters.to.save <- RJAGSprepfull [[3]]
 model.file <- RJAGSprepfull [[4]] #already defined?
-n.iter <- 50000; n.burnin <- 25000; n.thin <- 20; n.chains <- 1 #customize
+n.iter <- 50000; n.burnin <- 25000; n.thin <- 20; n.chains <- 1
 
-library(rjags)
+
+# #4 Execute RJAGS (call from this main workflow? Within function?)
+
+#library(rjags)
 #ex.jags<-jags(data=jags_data, inits=inits, parameters.to.save=parameters.to.save, model.file="UNADJ-jags-model.txt", n.chains=1, n.iter=50, n.burnin=10, n.thin=5)
 #ex.out<-ex.jags$BUGSoutput
 #str(ex.out$sims.list)
