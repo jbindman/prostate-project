5f1bdd37c3c71c939e428300cea2055b54f51aeb
Julia Bindman <jbindman@jhu.edu>
2016-04-12 01:57:12 -0400

    tuesday

--- R/workflow.R
+++ R/workflow.R
@@ -15,7 +15,7 @@ source("R/RJAGSprep.R")
 
 
 #######################
-# #1 Customize File Names
+# 1. Customize File Names
 
 demo_data<-read.csv("julia-demo-data.csv")
 psa_data<-read.csv("julia-psa-data.csv")
@@ -25,12 +25,12 @@ model.file <- "UNADJ-jags-model.txt"
 
 
 
-# #2 Prep for RJAGS
+# 2. Prep for RJAGS
 patientDataframes <- fillPatientTables(tx_data, demo_data, psa_data, bx_data)
 RJAGSprepfull <- RJAGSprep(patientDataframes, model.file) #returns list of arguments for running RJAGS
 
 
-# #3 Execute RJAGS (call from this main workflow? Within function?)
+# 3. Reextrat data from RJAGS Function (call from this main workflow? Within function? Save to workspace?)
 
 jags_data <- RJAGSprepfull [[1]]
 inits <- RJAGSprepfull [[2]]
@@ -39,7 +39,7 @@ model.file <- RJAGSprepfull [[4]] #already defined?
 n.iter <- 50000; n.burnin <- 25000; n.thin <- 20; n.chains <- 1
 
 
-# #4 Execute RJAGS (call from this main workflow? Within function?)
+# 4. Execute RJAGS (call from this main workflow? Within function?)
 
 #library(rjags)
 #ex.jags<-jags(data=jags_data, inits=inits, parameters.to.save=parameters.to.save, model.file="UNADJ-jags-model.txt", n.chains=1, n.iter=50, n.burnin=10, n.thin=5)
