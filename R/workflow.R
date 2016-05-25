bea4ca233f0de821b2533e271b26c251f456c2b7
Julia Bindman <jbindman@jhu.edu>
2016-05-25 00:13:52 -0400

    test

--- R/workflow.R
+++ R/workflow.R
@@ -24,7 +24,7 @@ tx_data<-read.csv("julia-tx-data.csv")
 
 # still giving me problems
 
-ProstatePackage:::writeModel() #default name can be overwritten here
+#ProstatePackage:::writeModel() #default name can be overwritten here
 
 
 # Organize data frames from clinical patient sources
