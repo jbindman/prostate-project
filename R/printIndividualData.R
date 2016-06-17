85c7921ee9ea041038338dbfc5347d45a51afe9c
Julia Bindman <jbindman@jhu.edu>
2016-06-17 00:37:12 -0400

    better print

--- R/printIndividualData.R
+++ R/printIndividualData.R
@@ -7,8 +7,8 @@
 #' @export
 printIndividualData<- function(idInput = 10, patientDataframes = ptDataframes) {
   pt.data <- patientDataframes[[1]] #global variable patientDataframes put into temporary dataframes with names matching RJAGS prep
-  psa.data <- patientDataframes[[2]]
-  bx.data <- patientDataframes[[3]]
+  #psa.data <- patientDataframes[[2]]
+  #bx.data <- patientDataframes[[3]]
 
   lapply(psa.data, class)
 
@@ -18,7 +18,7 @@ printIndividualData<- function(idInput = 10, patientDataframes = ptDataframes) {
   #bx.data[bx.data$id == patient,]
   library(dplyr)
 
-  print("Demographics Data: ")
+  #print("Demographics Data: ")
   formattedDemo <- filter(pt.data, id == idInput)
   formattedDemo$subj <- NULL
   formattedDemo$total.fup <- NULL
@@ -26,42 +26,63 @@ printIndividualData<- function(idInput = 10, patientDataframes = ptDataframes) {
   names(formattedDemo) <- c("Patient", "Gleason Score", "DOB", "Diagnosis Date", "Age at Diagnosis", "Avg Volume", "RC")
   print(formattedDemo)
 
-  print("PSA Data: ")
-  formattedPsa <- filter(psa.data, id == idInput)
-  formattedPsa <- arrange(formattedPsa, psa.date.num) #already in order but double check
-  formattedPsa <- formattedPsa[c("id", "psa", "psa.date.num", "age")]#add age
-  names(formattedPsa) <- c("Patient", "PSA", "Visit", "Age")
-  print(formattedPsa)
+  #print("PSA Data: ")
+  formattedPsa <- filter(psa_data, id == idInput)
+  #formattedPsa <- arrange(formattedPsa, psa.date.num) #already in order but double check
+  formattedPsa <- formattedPsa[c("psa", "psa.date")]#add age
+  names(formattedPsa) <- c("PSA", "Visit")
+  #print(formattedPsa)
 
-  print("Biopsy Data: ")
+  #print("Biopsy Data: ")
   #add id back to biopsy in fillPatientTables
-  formattedBx <- filter(bx.data, id == idInput)
-  formattedBx <- filter(formattedBx, bx.here == 1)
-  formattedBx <- arrange(formattedBx, bx.date.num)
-  formattedBx <- formattedBx[c("id", "bx.here", "bx.date.num", "bx.age")]
-  names(formattedBx) <- c("Patient", "Biopsy", "Visit", "Age")
-  print(formattedBx)
+  formattedBx <- filter(bx_data, id == idInput)
+  #formattedBx <- arrange(formattedBx, bx.date.num)
+  formattedBx <- formattedBx[c("bx.date", "RC", "vol")]
+  names(formattedBx) <- c("Visit", "RC", "vol")
+  #print(formattedBx)
 
 
-  merged.data <- merge(formattedPsa, formattedBx, by=c("Patient","Visit", "Age"), all = TRUE)
+  merged.data <- merge(formattedPsa, formattedBx, by=c("Visit"), all = TRUE)
   #merged.data[is.na(merged.data)] <- 0
 
   #all NA replaced with '-'
   for (i in 1:nrow(merged.data)) {
-    if(is.na(merged.data$Biopsy[i])) {
-      merged.data$Biopsy[i] <- "-"
+    #adjusting PSA to 2 decimals
+    PSAval <- as.numeric(merged.data$PSA[i])
+    PSAsmall <- round(PSAval, digits = 2)
+    merged.data$PSA[i] <- PSAsmall
+    #adjusting vol to 2 decimals
+    volVal <- as.numeric(merged.data$vol[i])
+    volSmall <- round(volVal, digits = 2)
+    merged.data$vol[i] <- volSmall
+
+    if(is.na(merged.data$RC[i])) {
+      merged.data$RC[i] <- "-"
+    }
+
+    if (merged.data$RC[i] == 0) {
+      merged.data$RC[i] <- "N"
+    }
+    else if (merged.data$RC[i] == 1) {
+      merged.data$RC[i] <- "Y"
+    }
+    if(is.na(merged.data$vol[i])) {
+      merged.data$vol[i] <- "-"
     }
     if(is.na(merged.data$PSA[i])) {
       merged.data$PSA[i] <- "-"
     }
+    #merged.data$PSA[i]
   }
+
+
   #for (i in 1:nrow(merged.data)) {
   #  if(merged.data$Biopsy == 1) {
   #    merged.data$Biopsy <-"Yes"
   #  }
   #}
-  merged.data$Visit <- as.Date(merged.data$Visit[1:nrow(merged.data)], origin="1970-01-01")
-  merged.data$Patient <- NULL #dont need to show anymore
+  #merged.data$Visit <- as.Date(merged.data$Visit[1:nrow(merged.data)], origin="1970-01-01")
+  #merged.data$Patient <- NULL #dont need to show anymore
 
 
 
@@ -69,5 +90,7 @@ printIndividualData<- function(idInput = 10, patientDataframes = ptDataframes) {
   print(merged.data)
 
   #ages are messed up, thinks biopsy at later time has younger patient age
+
+  #find differences between IOP and non IOP versions and add T/F option
 
 }
